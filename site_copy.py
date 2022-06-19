34ebf09ab3aba77ea51ef58690ffdce003caa366
ryanbanks-tech <blackwolf_alexander@hotmail.com>
2022-06-18 23:04:04 -0500

    Adding python file to copy website

---
+++ site_copy.py
@@ -0,0 +1,12 @@
+#! usr/bin/venv python3
+
+from pywebcopy import save_webpage
+
+kwargs = {'project_name': 'Nick Russo Site'}
+
+save_webpage(
+  url = 'http://njrusmc.net/',
+
+  project_folder = 'Copied Sites',
+  **kwargs
+)
