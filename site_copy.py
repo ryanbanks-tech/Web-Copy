c24563b7fc93a33b8d307f67b04f876b485c4519
ryanbanks-tech <blackwolf_alexander@hotmail.com>
2022-06-19 12:03:42 -0500

    Added a git ignore gile

--- site_copy.py
+++ site_copy.py
@@ -2,11 +2,11 @@
 
 from pywebcopy import save_webpage
 
-kwargs = {'project_name': 'Nick Russo Site'}
+kwargs = {'project_name': 'Nick-Russo-Site'}
 
 save_webpage(
   url = 'http://njrusmc.net/',
 
-  project_folder = 'Copied Sites',
+  project_folder = 'Copied-Sites/',
   **kwargs
 )
